b831056a9acfa28fd1d128fcc35e1a4b4bae8d9c
Rafael Hengles <rhengles@gmail.com>
2014-04-27 15:19:03 -0300

    refactoring

--- es6/jpopsuki/spider/getindex.js
+++ es6/jpopsuki/spider/getindex.js
@@ -1,36 +1,20 @@
-import hp from 'htmlparser2';
 import request from './requestjps';
 
 function getIndex(cookies, cb, log, getBody) {
-	var body = ''
-		, handler = new hp.DomHandler(function(err, dom) {
-				if ( err ) {
-					console.log('Error parsing '+req.info());
-					console.log(err);
-					return;
-				}
-				cb.call(req, dom, response, req);
-			})
-		, parser = new hp.Parser(handler)
-		, opt =
+	var opt =
       { path: '/index.php'
 			, head:
 				{ 'Cookie': cookies
 				, 'Referer': 'http://jpopsuki.eu/login.php'
 				}
-			, onData: function(chunk) {
-					parser.write(chunk);
-					getBody && (body += chunk);
-				}
+			, getBody: getBody
+			, getDom: true
 			, done: function(res, req) {
-					getBody && (res.body = body);
-					response = res;
-					parser.done();
+					cb(res.dom, res, req);
 				}
 			, log: log
       }
-		, req = request(opt)
-		, response;
+		, req = request(opt);
 	return req;
 }
 
