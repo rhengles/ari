95e9b5f49563bde16e366bb528041a27bf920e9e
Rafael Hengles <rhengles@gmail.com>
2018-05-15 17:51:58 +0000

    Allow OPTIONS & headers

--- backend/proxy.js
+++ backend/proxy.js
@@ -6,18 +6,26 @@ var express = require('express');
 var httpProxy = require('express-http-proxy');
 var url = require('url');
 
+var proxyConfig = {};
+
+if (process.argv[4]) {
+	proxyConfig.limit = process.argv[4];
+}
+
 var app = express();
 
 var port = process.argv[2];
 
-var proxyServer = httpProxy(process.argv[3]/*, {
-	proxyReqPathResolver: function(req, res) {
-		return url.parse(req.originalUrl).path;
-	}
-}*/);
+var proxyServer = httpProxy(process.argv[3], proxyConfig);
 
 app.use(function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+	res.setHeader('Access-Control-Max-Age', '86400');
+	if (req.method.toUpperCase() === 'OPTIONS') {
+		return res.status(200).end();
+	}
 	next();
 });
 
